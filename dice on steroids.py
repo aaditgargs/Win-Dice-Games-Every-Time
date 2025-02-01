66699033c0d6b66fd7ced2d280337f1e99dac47e
aaditgargs <141548884+aaditgargs@users.noreply.github.com>
2025-02-01 22:05:22 +0530

    dice on steroids.py

--- dice on steroids.py
+++ dice on steroids.py
@@ -1,13 +1,16 @@
 import random
 
-num_going = True
+num_goin3g = True
 time_num = 0
 end = 0
 
+#change this number to edit the number of times you require your numbers
 while time_num <= 4:
     while num_going == True:
+        #change this number if you want different types of dice
         num = random.randint(1, 6)
         time_num += 1
+        #change this number to edit the numbers you want
         if num <= 3:
             print(num)
         else:
